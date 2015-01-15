8003dfdf40d34085a44efa5a268bb07995a28020
Hannes Simon Güdelhöfer <uz66agex@clientmaster.rbg.informatik.tu-darmstadt.de>
2015-01-15 12:44:07 +0100

    all

---
+++ all.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+
+echo "\nbuilding without anything"
+cmake -DBUILD_QT=OFF -DUSE_OPENMP=OFF > /dev/null &&
+make clean > /dev/null &&
+make > /dev/null &&
+
+echo "\nnbody:"
+./bin/praktikum4 nbody.txt nbody_out.txt 
+echo "\nrandom:"
+./bin/praktikum4 random.txt random_out.txt
+echo "\ncollision:"
+./bin/praktikum4 collision.txt collision_out.txt
+
+
+echo "\nbuilding with OpenMP"
+cmake -DBUILD_QT=OFF -DUSE_OPENMP=ON > /dev/null &&
+make clean > /dev/null &&
+make > /dev/null &&
+
+echo "\nnbody:"
+./bin/praktikum4 nbody.txt nbody_MP_out.txt 
+echo "\nrandom:"
+./bin/praktikum4 random.txt random_MP_out.txt
+echo "\ncollision:"
+./bin/praktikum4 collision.txt collision_MP_out.txt
